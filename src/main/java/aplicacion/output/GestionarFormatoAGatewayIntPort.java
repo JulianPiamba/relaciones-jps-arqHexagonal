726a7569fdfa4f6995c316ae22f44db401eb759f
freider45 <freiderescobar@unicauca.edu.co>
2025-04-28 15:52:44 -0500

    Agregando gateways

--- src/main/java/aplicacion/output/GestionarFormatoAGatewayIntPort.java
+++ src/main/java/aplicacion/output/GestionarFormatoAGatewayIntPort.java
@@ -1,5 +1,10 @@
 package aplicacion.output;
 
+import dominio.modelos.FormatoPPA;
+import dominio.modelos.FormatoTIA;
+
 public interface GestionarFormatoAGatewayIntPort {
     
+    public FormatoPPA guardarFormatoPPA(FormatoPPA formatoppa);
+    public FormatoTIA guardarFormatoTIA(FormatoTIA formatotia);
 }
--- src/main/java/infraestructura/output/persistencia/gateway/GestionarFormatoAGatewayImplAdapter.java
+++ src/main/java/infraestructura/output/persistencia/gateway/GestionarFormatoAGatewayImplAdapter.java
@@ -1,7 +1,21 @@
 package infraestructura.output.persistencia.gateway;
 
 import aplicacion.output.GestionarFormatoAGatewayIntPort;
+import dominio.modelos.FormatoPPA;
+import dominio.modelos.FormatoTIA;
 
 public class GestionarFormatoAGatewayImplAdapter implements GestionarFormatoAGatewayIntPort {
+
+    @Override
+    public FormatoPPA guardarFormatoPPA(FormatoPPA formatoppa) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'guardarFormatoPPA'");
+    }
+
+    @Override
+    public FormatoTIA guardarFormatoTIA(FormatoTIA formatotia) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'guardarFormatoTIA'");
+    }
     
 }
