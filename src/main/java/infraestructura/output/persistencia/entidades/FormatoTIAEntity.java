707176d5a8b64d28bf0b19168c6b68d5d8415d43
freider45 <freiderescobar@unicauca.edu.co>
2025-04-28 14:54:37 -0500

    Agrega entidades en infraestructura

---
+++ src/main/java/infraestructura/output/persistencia/entidades/FormatoTIAEntity.java
@@ -0,0 +1,40 @@
+package infraestructura.output.persistencia.entidades;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.PrimaryKeyJoinColumn;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "FormatosTIA")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@PrimaryKeyJoinColumn(name = "idFormatoTIA")
+public class FormatoTIAEntity extends FormatoAEntity {
+
+    @Column(name = "nombre_estudiante1", nullable = false, length = 100)
+    private String nombreEstudiante1;
+    @Column(name = "nombre_estudiante2", nullable = true, length = 100)
+    private String nombreEstudiante2;
+    
+    public FormatoTIAEntity(String titulo, String objetivoGeneral, String objetivosEspecificos,
+     String nombreEstudiante1, String nombreEstudiante2)
+    {
+        super(titulo, objetivoGeneral, objetivosEspecificos);
+        this.nombreEstudiante1 = nombreEstudiante1;
+        this.nombreEstudiante2 = nombreEstudiante2;
+    }
+
+    public FormatoTIAEntity(String titulo, String objetivoGeneral, String objetivosEspecificos,
+     String nombreEstudiante1)
+    {
+        super(titulo, objetivoGeneral, objetivosEspecificos);
+        this.nombreEstudiante1 = nombreEstudiante1;
+    }
+}
---
+++ src/main/java/infraestructura/output/persistencia/entidades/RolEntity.java
@@ -0,0 +1,38 @@
+package infraestructura.output.persistencia.entidades;
+
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "Roles")
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class RolEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @Column(unique = true,nullable = false)
+    private int idRol;
+    @Column(name = "role_asignado", nullable = false, length = 100)
+    private String roleAsignado;
+
+    //Un rol tiene muchos historicos
+    @OneToMany(fetch = FetchType.EAGER, mappedBy = "objRol")
+    private List<HistoricoEntity> historicos;
+    
+}
